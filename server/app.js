d77e8a7f604ed6b2e0364340d5e5436a7268509a
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-11-01 12:13:50 -0700

    Remove body-parser

--- server/app.js
+++ server/app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs');
-const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/../client/'));
 
 let dictionary;
